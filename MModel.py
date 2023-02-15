1250f08e296e79c0a9e225c0e71bc53abc0970b9
frkngny <53319214+frkngny@users.noreply.github.com>
2023-02-15 19:31:42 +0300

    update 15022023

--- MModel.py
+++ MModel.py
@@ -1,3 +1,6 @@
+import copy
+
+from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.arima.model import ARIMA
 import pandas as pd
 import numpy as np
@@ -17,14 +20,17 @@ class InitModel:
         self.l_ratio = "L_ratio"
         self.train_data = None
         self.test_data = None
+        self.output_data = None
+        self.model_train = None
         self.model = None
         self.predicted = None
-        self.buys = []
+        self.buys = {"times": [], "prices": []}
         self.buy_percents = []
         self.sells = []
-        self.budget = 250000    # edit this initial budget to buy
+        self.budget = 250000  # edit this initial budget to buy
         self.pf = 1.5
         self.total_profit = 0
+        self.output_stored = "output_stored.csv"
 
     def create_train_and_test(self):
         # Read dataset
@@ -43,12 +49,14 @@ class InitModel:
         test_data[self.l_ratio] = test_data[self.ratio_col] / (1 + test_data[self.ratio_col])
 
         self.train_data = data
-        self.test_data = test_data
+        self.test_data = test_data.iloc[-1:]
 
     def create_model(self):
         y = self.train_data[self.l_ratio]
-        self.model = ARIMA(y, order=(8, 0, 7))
-        self.model = self.model.fit()
+        self.model_train = ARIMA(y, order=(8, 0, 7))
+        self.model_train = self.model_train.fit()
+        self.model = copy.deepcopy(self.model_train)
+        self.update_train_data()
 
     def create_prediction(self):
         y_pred = self.model.get_forecast(len(self.test_data.index))
@@ -56,6 +64,8 @@ class InitModel:
         y_pred_df["Predictions"] = self.model.predict(start=y_pred_df.index[0], end=y_pred_df.index[-1])
         y_pred_df.index = self.test_data.index
         self.predicted = y_pred_df["Predictions"]
+        arma_rmse = np.sqrt(mean_squared_error(self.test_data[self.l_ratio].values, y_pred_df["Predictions"]))
+        print(arma_rmse)
 
     def create_buy_sell(self):
         self.create_prediction()
@@ -63,48 +73,50 @@ class InitModel:
         self.test_data.index = [i for i in range(len(self.test_data.index))]
         self.test_data.to_csv(self.output_csv, index=False)
 
+        stored = pd.read_csv(self.output_stored)
+        stored = pd.concat([stored, self.test_data])
+        stored.to_csv(self.output_stored, index=False)
+
     def update_train_data(self):
-        self.train_data += self.test_data
+        self.train_data = pd.concat([self.train_data, self.test_data])
         self.train_data.to_csv(self.train_csv, index=False)
 
     def create_profit_factor(self):
         data = pd.read_csv(self.output_csv)
+        stored_data = pd.read_csv(self.output_stored)
+
         for i in range(len(data)):
             data_price = data[self.price_col][i]
+            data_time = data[self.time_col][i]
             data_signal = data['Signal'][i]
             if data_signal == "buy":
                 if data_price < self.budget:
-                    self.buys.append(data_price)
+                    self.buys["prices"].append(data_price)
+                    self.buys["times"].append(data_time)
                     self.budget -= data_price
             else:
-                if len(self.buys) > 0:
-                    profit_factor, profits = calculate_profit_factor(self.buys, data_price)
+                if len(self.buys["prices"]) > 0:
+                    profit_factor, profits = calculate_profit_factor(self.buys["prices"], data_price)
                     if profit_factor >= self.pf:
-                        buy_copy = self.buys.copy()
+                        buy_copy = copy.deepcopy(self.buys)
                         profits_copy = profits.copy()
                         for k in range(len(profits)):
                             profit_mean = np.average(profits)
                             if profits[k] >= profit_mean:
-                                print(f"Sell {self.buys[k]} at {data_price} with profit: {profits[k]}")
-                                buy_copy.remove(self.buys[k])
+                                print(f"Sell {self.buys['prices'][k]} at {data_price} with profit: {profits[k]}")
+                                stored_data.loc[
+                                    stored_data[self.time_col] == self.buys['times'][k], "Sold_At"] = int(data_time)
+                                buy_copy['prices'].remove(self.buys['prices'][k])
+                                buy_copy['times'].remove(self.buys['times'][k])
+
                                 profits_copy.remove(profits[k])
                                 self.total_profit += profits[k]
                                 self.budget += data_price
                         self.buys = buy_copy
                         self.sells.append(data_price)
-        print(self.total_profit)
-        print(self.buys)
-        print(self.budget)
 
+                        print("Total profit: ", self.total_profit)
+                        print("Current buy prices: ", self.buys)
+                        print("Current budget: ", self.budget)
 
-if __name__ == "__main__":
-    initer = InitModel()
-    # first run
-    initer.create_train_and_test()
-    initer.create_model()
-    initer.create_buy_sell()
-
-    # loop
-    initer.update_train_data()
-    initer.create_model()
-    initer.create_buy_sell()
+        stored_data.to_csv(self.output_stored, index=False)
