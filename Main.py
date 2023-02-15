10c2ef122e6bfe49795c6376c91a6fb858f97471
frkngny <53319214+frkngny@users.noreply.github.com>
2023-02-15 19:32:12 +0300

    update 15022023

--- Main.py
+++ Main.py
@@ -11,9 +11,9 @@ class Main:
         self.price_col = "price"
         self.ratio_col = "ls_ratio_overall"
         self.l_ratio = "L_ratio"
-        self.train_csv = "training_data.csv"
-        self.test_csv = "input_data.csv"
-        self.output_csv = "output.csv"
+        self.train_csv = "training_data2.csv"
+        self.test_csv = "input_data2.csv"
+        self.output_csv = "output2.csv"
         self.initModel = InitModel(self.time_col, self.price_col, self.ratio_col, self.train_csv, self.test_csv,
                                    self.output_csv)
         self.dataMngr = DataHandler(self.test_csv)
@@ -25,17 +25,35 @@ class Main:
         self.initModel.create_profit_factor()
 
     def init_process(self):
-        start_time = time.perf_counter()
+        ctr = True
+        while True:
+            try:
+                start_time = time.perf_counter()
 
-        self.dataMngr.collect_data_and_store()
-        self.async_proc()
+                self.dataMngr.collect_data_and_store()
+                if ctr:
+                    self.async_proc()
+                else:
+                    Thread(target=self.async_proc_model()).start()
+                    self.async_proc_signal()
 
-        end_time = time.perf_counter()
-        if (start_time - end_time) > self.time_interval:
-            time.sleep(self.time_interval)
-        else:
-            wait = self.time_interval - (start_time - end_time)
-            time.sleep(wait)
+                end_time = time.perf_counter()
+                if (start_time - end_time) > self.time_interval:
+                    time.sleep(self.time_interval)
+                else:
+                    wait = self.time_interval - (start_time - end_time)
+                    time.sleep(wait)
+            except:
+                continue
+            ctr = False
+
+    def async_proc_model(self):
+        self.initModel.create_train_and_test()
+        self.initModel.create_model()
+
+    def async_proc_signal(self):
+        self.initModel.create_buy_sell()
+        self.initModel.create_profit_factor()
 
     def async_proc(self):
         """
@@ -45,7 +63,6 @@ class Main:
         self.initModel.create_model()
         self.initModel.create_buy_sell()
         self.initModel.create_profit_factor()
-        self.initModel.update_train_data()
 
     def start_process(self):
         thrd = Thread(target=self.init_process())
